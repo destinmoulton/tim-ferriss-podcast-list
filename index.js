7a4fa047dc1b302f190d4fcaff9da10d58a72efb
Destin Moulton <destin.moulton@gmail.com>
2017-09-05 16:18:18 -0600

    Added nunjucks and a buildHTML method.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const fs = require("fs");
+
 const feedparser = require("feedparser-promised");
+const nunjucks = require("nunjucks");
 
 const URL = "http://timferriss.libsyn.com/rss";
 
 feedparser.parse(URL)
     .then((items)=>{
-        items.forEach((item)=>{
-            console.log(item.title);
-        });
+        buildHTML(items);
     })
     .catch((err)=>{
         console.error(err);
     })
+
+function buildHTML(items){
+    const template = "./templates/body.njk";
+    const html = nunjucks.render(template, {items: items});
+
+    fs.writeFile("index.html", html, (err)=>{
+        if(err) {
+            return console.error("Unable to create index.html!");
+        }
+
+        console.log("index.html created successfully!")
+    });
+}

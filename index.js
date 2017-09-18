49603899277f03fd739fe30abf3dd80682e2d009
Destin Moulton <destin.moulton@gmail.com>
2017-09-18 13:59:47 -0600

    Begin migration to typescript.

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
-const fs = require("fs");
-
-const feedparser = require("feedparser-promised");
-const nunjucks = require("nunjucks");
-
-const URL = "http://timferriss.libsyn.com/rss";
-
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var feedparser = require("feedparser-promised");
+var nunjucks = require("nunjucks");
+var URL = "http://timferriss.libsyn.com/rss";
 feedparser.parse(URL)
-    .then((items)=>{
-        buildHTML(items);
-    })
-    .catch((err)=>{
-        console.error(err);
-    })
-
-function buildHTML(items){
-    const template = "./templates/body.njk";
-    const html = nunjucks.render(template, {items: processItems(items)});
-
-    fs.writeFile("index.html", html, (err)=>{
-        if(err) {
+    .then(function (items) {
+    buildHTML(items);
+})["catch"](function (err) {
+    console.error(err);
+});
+function buildHTML(items) {
+    var template = "./templates/body.njk";
+    var html = nunjucks.render(template, { items: processItems(items) });
+    fs.writeFile("index.html", html, function (err) {
+        if (err) {
             return console.error("Unable to create index.html!");
         }
-
-        console.log("index.html created successfully!")
+        console.log("index.html created successfully!");
     });
 }
-
-function processItems(origItems){
-    let episodeCount = origItems.length;
-
-    let processedItems = [];
-    origItems.forEach((item)=>{
-        const newItem = Object.assign({}, item);
-        const titleInfo = stripEpisodeNumber(item.title);
-
+function processItems(origItems) {
+    var episodeCount = origItems.length;
+    var processedItems = [];
+    origItems.forEach(function (item) {
+        var newItem = Object.assign({}, item);
+        var titleInfo = stripEpisodeNumber(item.title);
         newItem.title = titleInfo.title;
         newItem.episodeNumber = titleInfo.episodeNumber;
-
         processedItems.push(newItem);
         episodeCount--;
     });
-
     return processedItems;
 }
-
-function stripEpisodeNumber(title){
-    let episodeNumber = 0;
-    
-    let episodeTitle = title;
-    let colonIndex = title.indexOf(":");
-    if(colonIndex > -1){
+function stripEpisodeNumber(title) {
+    var episodeNumber = 0;
+    var episodeTitle = title;
+    var colonIndex = title.indexOf(":");
+    if (colonIndex > -1) {
         episodeTitle = title.substring(colonIndex + 2);
     }
-    
-    if(title.indexOf("#") === 0){
+    if (title.indexOf("#") === 0) {
         episodeNumber = parseInt(title.substring(1, colonIndex + 1));
-    } else if(title.startsWith("Episode ")){
+    }
+    else if (title.startsWith("Episode ")) {
         episodeNumber = parseInt(title.substring(7, colonIndex + 1));
-    } else if(title.startsWith("Ep. ")){
+    }
+    else if (title.startsWith("Ep. ")) {
         episodeNumber = parseInt(title.substring(3, colonIndex + 1));
-    } else if(title.startsWith("Ep ")){
+    }
+    else if (title.startsWith("Ep ")) {
         episodeNumber = parseInt(title.substring(2, colonIndex + 1));
     }
-
-    if(isNaN(episodeNumber)){
+    if (isNaN(episodeNumber)) {
         episodeNumber = 0;
     }
-    return {title: episodeTitle, episodeNumber: episodeNumber};
+    return { title: episodeTitle, episodeNumber: episodeNumber };
 }
---
+++ index.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+
+import feedparser from "feedparser-promised";
+import nunjucks from "nunjucks";
+
+const URL = "http://timferriss.libsyn.com/rss";
+
+feedparser.parse(URL)
+    .then((items)=>{
+        buildHTML(items);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+
+function buildHTML(items){
+    const template = "./templates/body.njk";
+    const html = nunjucks.render(template, {items: processItems(items)});
+
+    fs.writeFile("index.html", html, (err)=>{
+        if(err) {
+            return console.error("Unable to create index.html!");
+        }
+
+        console.log("index.html created successfully!")
+    });
+}
+
+function processItems(origItems){
+    let episodeCount = origItems.length;
+
+    let processedItems = [];
+    origItems.forEach((item)=>{
+        const newItem = Object.assign({}, item);
+        const titleInfo = stripEpisodeNumber(item.title);
+
+        newItem.title = titleInfo.title;
+        newItem.episodeNumber = titleInfo.episodeNumber;
+
+        processedItems.push(newItem);
+        episodeCount--;
+    });
+
+    return processedItems;
+}
+
+function stripEpisodeNumber(title){
+    let episodeNumber = 0;
+    
+    let episodeTitle = title;
+    let colonIndex = title.indexOf(":");
+    if(colonIndex > -1){
+        episodeTitle = title.substring(colonIndex + 2);
+    }
+    
+    if(title.indexOf("#") === 0){
+        episodeNumber = parseInt(title.substring(1, colonIndex + 1));
+    } else if(title.startsWith("Episode ")){
+        episodeNumber = parseInt(title.substring(7, colonIndex + 1));
+    } else if(title.startsWith("Ep. ")){
+        episodeNumber = parseInt(title.substring(3, colonIndex + 1));
+    } else if(title.startsWith("Ep ")){
+        episodeNumber = parseInt(title.substring(2, colonIndex + 1));
+    }
+
+    if(isNaN(episodeNumber)){
+        episodeNumber = 0;
+    }
+    return {title: episodeTitle, episodeNumber: episodeNumber};
+}
